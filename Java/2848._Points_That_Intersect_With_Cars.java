e73af75167ac325c1eefc4a9b761d74160bc92f4
Ravi B. Dhavare <100593775+ravidhavare111@users.noreply.github.com>
2023-10-11 13:03:07 +0530

    Create 2848._Points_That_Intersect_With_Cars.java

---
+++ Java/2848._Points_That_Intersect_With_Cars.java
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/points-that-intersect-with-cars/description/
+
+class Solution {
+    public int numberOfPoints(List<List<Integer>> nums) {
+
+        int count = 0 ;
+        int n = nums.size();
+        Set<Integer> set = new HashSet<>();
+
+        for(List<Integer> temp : nums){
+            
+            int s = temp.get(0);
+            int e = temp.get(1);
+
+            for(int i = s ; i<=e ; i++){
+                set.add(i);
+            }
+        }
+        return set.size();
+    }
+} 
