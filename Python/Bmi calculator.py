faca8bb5fd686f447f07c8dbe868d4fadc73a5c3
Oscar Kandir <110010835+OscarK-coder@users.noreply.github.com>
2023-10-16 11:59:35 +0000

    made some modifications

--- Python/Bmi calculator.py
+++ Python/Bmi calculator.py
@@ -1,19 +1,25 @@
-def calculate_bmi(weight, height):
-    return weight / (height * height)
+def determine_weight_status(weight, height):
+    bmi = weight / (height * height)
+    if bmi < 16.0:
+        return "severely underweight"
+    elif 16.0 <= bmi < 16.9:
+        return "very underweight"
+    elif 17.0 <= bmi < 18.4:
+        return "moderately underweight"
+    elif 18.5 <= bmi < 24.9:
+        return "normal weight"
+    elif 25.0 <= bmi < 29.9:
+        return "overweight"
+    elif 30.0 <= bmi < 34.9:
+        return "obese class 1 (Moderate)"
+    elif 35.0 <= bmi < 39.9:
+        return "obese class 2 (Severe)"
+    else:
+        return "obese class 3 (Very Severe)"
 
 weight = float(input("Enter your weight in kilograms: "))
-
 height = float(input("Enter your height in meters: "))
 
-bmi = calculate_bmi(weight, height)
+status = determine_weight_status(weight, height)
+print(f"You are {status}.")
 
-print("Your BMI is: {:.2f}".format(bmi))
-
-if bmi < 18.5:
-    print("You are underweight.")
-elif 18.5 <= bmi < 24.9:
-    print("You have a normal weight.")
-elif 25 <= bmi < 29.9:
-    print("You are overweight.")
-else:
-    print("You are obese.")
